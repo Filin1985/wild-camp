645b88a768a845b10522eb48b2ce91e95b86394c
Marat Ikhsanov <ihsmen@gmail.com>
2024-08-31 12:14:36 +0300

    Finish Dashboard page

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -6,6 +6,7 @@ import {useCabins} from "../cabins/useCabins"
 import Stats from "./Stats"
 import SalesChart from "./SalesChart"
 import DurationChart from "./DurationChart"
+import TodayActivity from "../check-in-out/TodayActivity"
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -29,8 +30,8 @@ function DashboardLayout() {
         numDays={numDays}
         cabinsCount={cabins.length}
       />
-      <div>Hello</div>
-      <DurationChart confirmedStays={confirmedStays}/>
+      <TodayActivity />
+      <DurationChart confirmedStays={confirmedStays} />
       <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
   )
